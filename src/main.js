9d416defef352aa5e515e9c9645eb9da4b06d3e3
rainbowchen <1664373735@qq.com>
2023-04-19 13:50:38 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+// 引入路由
+import router from './router'
 Vue.config.productionTip = false
+// 全局组件，三级联动组件,直接使用
+import TypeNav from '@/pages/Home/TypeNav'
+Vue.component(TypeNav.name, TypeNav)
+
 
 new Vue({
+  // 注册路由,所有组件都拥有$route和$router
+  router,
   render: h => h(App),
 }).$mount('#app')
