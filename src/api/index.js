f789812c103b2bbdbdc57d5e087ff4b058c85a3a
rainbowchen <1664373735@qq.com>
2023-05-01 18:40:52 +0800

    我的订单主页面动态完成

--- src/api/index.js
+++ src/api/index.js
@@ -58,3 +58,5 @@ export const reqSubmitOrder = (tradeNo,data)=>requests({url:`/order/auth/submitO
 export const reqPayInfo = (orderId)=>requests({url:`/payment/weixin/createNative/${orderId}`,method:'get'});
 // 获取订单支付状态
 export const reqPayState = (orderId)=>requests({url:`/payment/weixin/queryPayStatus/${orderId}`,method:'get'});
+// 获取我的订单列表
+export const reqMyOrderList = (page,limit)=>requests({url:`/order/auth/${page}/${limit}`,method:'get'});
