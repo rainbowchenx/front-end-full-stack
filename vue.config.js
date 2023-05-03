3eb4fac8890bf16aa4f8b9fbf63f9be8555ca677
rainbowchen <1664373735@qq.com>
2023-05-03 16:49:23 +0800

    none

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  productionSourceMap:false,
   transpileDependencies: true,
   lintOnSave:false,//防止频繁报错
   // 配置代理服务器，解决跨域
